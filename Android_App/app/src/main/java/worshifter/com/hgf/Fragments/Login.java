4618e705eb1afd28e3de9775b6d40a07cf131fc1
guy <guyluz11@gmail.com>
2019-03-15 02:45:43 +0200

    Add font and demonstration how to change page (fragment)

--- Android_App/app/src/main/java/worshifter/com/hgf/Fragments/Login.java
+++ Android_App/app/src/main/java/worshifter/com/hgf/Fragments/Login.java
@@ -3,15 +3,19 @@ package worshifter.com.hgf.Fragments;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import worshifter.com.hgf.R;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class Login extends Fragment {
     private View myView;
+    private Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,10 +24,20 @@ public class Login extends Fragment {
         myView = inflater.inflate(R.layout.fragment_login, container, false);   // get the view of the fragment (do not delete or move)
         constructViews();
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("go", "This is button clicked");
+                Navigation.findNavController(myView).navigate(R.id.action_login_to_createNewAccount);  // Change to Chef kitchen Screen
+            }
+        });
         return myView;
     }
 
 
     private void constructViews() {        // initialize all the variables in an organized way
+
+        button = myView.findViewById(R.id.button);
+
     }
 }
